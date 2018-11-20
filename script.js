7970a7ed8be50a328946be8ba1f2fd0828d3a1f0
Zeke <zeke.cyr@comcast.net>
2018-11-20 14:20:15 -0700

    js started.

---
+++ script.js
@@ -0,0 +1,25 @@
+document.addEventListener("DOMContentLoaded", function(){
+
+    var currentColor 
+
+    var canvasHolder = document.getElementById("canvas")
+    var paletteHolder = document.getElementById("palette")
+
+    console.log(canvasHolder)
+    console.log(paletteHolder)
+
+    canvasHolder.addEventListener("click", changeColor)
+
+    function changeColor(){
+        
+    }
+
+    
+
+
+
+
+
+
+
+})
