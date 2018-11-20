ebd9e544684d20eb59d1ea115099ac47b243339b
Zeke <zeke.cyr@comcast.net>
2018-11-20 14:47:43 -0700

    built the js color selection script (which works with no other work apparently?

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function(){
 
-    var currentColor 
+    var currentColor = "white"
 
     var canvasHolder = document.getElementById("canvas")
     var paletteHolder = document.getElementById("palette")
@@ -8,18 +8,29 @@ document.addEventListener("DOMContentLoaded", function(){
     console.log(canvasHolder)
     console.log(paletteHolder)
 
-    canvasHolder.addEventListener("click", changeColor)
+    for(let i = 0;i < canvasHolder.getElementsByClassName("pixel").length;i++){
 
-    function changeColor(){
-        
+        canvasHolder.getElementsByClassName("pixel")[i].addEventListener("click", function() {
+
+            console.log("hey you clicked this div: " + i + " which is" + canvasHolder.childNodes[i])
+            var temp = canvasHolder.getElementsByClassName("pixel")[i]
+            temp.id = currentColor
+            
+        })
+    }
+
+
+    for(let i = 0;i < paletteHolder.getElementsByClassName("color").length;i++){
+
+        paletteHolder.getElementsByClassName("color")[i].addEventListener("click", function() {
+
+            console.log("hey you clicked this div: " + i + " which is" + canvasHolder.childNodes[i])
+            var temp = paletteHolder.getElementsByClassName("color")[i]
+            currentColor = temp.id
+            console.log(currentColor)
+            
+        })
     }
 
     
-
-
-
-
-
-
-
 })
