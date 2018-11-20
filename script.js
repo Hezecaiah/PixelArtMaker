fc15e092af1157f84768d3d75cbab76320877cf9
Zeke <zeke.cyr@comcast.net>
2018-11-20 15:43:20 -0700

    fixed up js and condensed class additions

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ document.addEventListener("DOMContentLoaded", function(){
     var canvasHolder = document.getElementById("canvas")
     var paletteHolder = document.getElementById("palette")
 
+    var colorsToAdd = ["white","red","orange","yellow","green","blue","indigo","violet"]
+
     var canvasWidth = 20
     var canvasLength = 20
     // var canvasWidth = window.prompt("how tall? (use reasonable numbers or i'll kill you(also nothing will show))")
@@ -15,27 +17,27 @@ document.addEventListener("DOMContentLoaded", function(){
 
         for(let j = 0;j < canvasWidth;j++){
             let tempBox = document.createElement("div")
-            tempBox.classList.add("pixel")
+            tempBox.classList.add("pixel","white")
             rowGen.appendChild(tempBox)
         }
-
         document.getElementById("canvas").appendChild(rowGen)
     }
 
-    //generate the colors here
-    
+    for(let i = 0;i < colorsToAdd.length;i++){
+        var newColor = document.createElement("div")
+        newColor.classList.add("color",colorsToAdd[i])
+        paletteHolder.appendChild(newColor)
+    }
 
     for(let i = 0;i < canvasHolder.getElementsByClassName("pixel").length;i++){
         canvasHolder.getElementsByClassName("pixel")[i].addEventListener("click", function() {
-            console.log("hey you clicked this div: " + i + " which is" + canvasHolder.childNodes[i])
             var temp = canvasHolder.getElementsByClassName("pixel")[i]
-            temp.classList.add(currentColor) = currentColor
+            temp.classList.replace(temp.classList[1],currentColor)
         })
     }
 
     for(let i = 0;i < paletteHolder.getElementsByClassName("color").length;i++){
         paletteHolder.getElementsByClassName("color")[i].addEventListener("click", function() {
-            console.log("hey you clicked this div: " + i + " which is" + canvasHolder.childNodes[i])
             var temp = paletteHolder.getElementsByClassName("color")[i]
             currentColor = temp.classList[1]
         })
