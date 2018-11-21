accc716c50d6f68cd73c00c2fc0f8cbc7c74b14a
Zeke <zeke.cyr@comcast.net>
2018-11-20 22:07:58 -0700

    code assessed, to do list added.

--- script.js
+++ script.js
@@ -9,7 +9,6 @@ document.addEventListener("DOMContentLoaded", function(){
 
     for(let i = 0;i < canvasLength;i++){
         var rowGen = document.createElement("div")
-
         for(let j = 0;j < canvasWidth;j++){
             let tempBox = document.createElement("div")
             tempBox.classList.add("pixel","white")
@@ -36,5 +35,16 @@ document.addEventListener("DOMContentLoaded", function(){
             var temp = paletteHolder.getElementsByClassName("color")[i]
             currentColor = temp.classList[1]
         })
-    }    
+    }
 })
+
+//TO DO: Move loops out of the onload as anonymous functions and declare them as functions. Per James:
+// If you want this to look even cleaner, moving all of your for loops out of the onload and making 
+// the functions then calling them inside the onload instead. Yours works great and looks good but if you want 
+// to make things easier to read and make things more manageable with larger code, divide you code into smaller
+// reusable functions.
+
+//Implement the color selector mentioned in the prompt to work around implementing every single color in css
+//Implement the other drawing methods (holding down and dragging, maybe filling) mentioned in the readme
+
+//Stretch: Local storage, saving paintings. Make the mouse pointer look like a brush.
