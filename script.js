fca0af05508f717808926fc7d65f28044f360695
Zeke <zeke.cyr@comcast.net>
2018-11-21 10:40:05 -0700

    defined anonymous functions within the onload as proper ones outside

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
+var currentColor = "white"
+var canvasHolder = document.getElementById("canvasFinder")
+var paletteHolder = document.getElementById("paletteFinder")
+var colorsToAdd = ["white","red","orange","yellow","green","blue","indigo","violet"]
+var canvasWidth = window.prompt("How tall? (Please use reasonable numbers for the time being.", 20)
+var canvasLength = window.prompt("How long?", 20)
+
+
 document.addEventListener("DOMContentLoaded", function(){
+    generateCanvas()
+    generatePalette()
+    assignClickListeners()
+    assignPaletteListeners()
+})
 
-    var currentColor = "white"
-    var canvasHolder = document.getElementById("canvasFinder")
-    var paletteHolder = document.getElementById("paletteFinder")
-    var colorsToAdd = ["white","red","orange","yellow","green","blue","indigo","violet"]
-    var canvasWidth = window.prompt("How tall? (Please use reasonable numbers for the time being.", 20)
-    var canvasLength = window.prompt("How long?", 20)
-
+function generateCanvas(){
     for(let i = 0;i < canvasLength;i++){
         var rowGen = document.createElement("div")
         for(let j = 0;j < canvasWidth;j++){
@@ -16,27 +23,33 @@ document.addEventListener("DOMContentLoaded", function(){
         }
         canvasHolder.appendChild(rowGen)
     }
+}
 
+function generatePalette(){
     for(let i = 0;i < colorsToAdd.length;i++){
         var newColor = document.createElement("div")
         newColor.classList.add("color",colorsToAdd[i])
         paletteHolder.appendChild(newColor)
     }
+}
 
+function assignClickListeners(){
     for(let i = 0;i < canvasHolder.getElementsByClassName("pixel").length;i++){
         canvasHolder.getElementsByClassName("pixel")[i].addEventListener("click", function() {
             var temp = canvasHolder.getElementsByClassName("pixel")[i]
             temp.classList.replace(temp.classList[1],currentColor)
         })
     }
+}
 
+function assignPaletteListeners(){
     for(let i = 0;i < paletteHolder.getElementsByClassName("color").length;i++){
         paletteHolder.getElementsByClassName("color")[i].addEventListener("click", function() {
             var temp = paletteHolder.getElementsByClassName("color")[i]
             currentColor = temp.classList[1]
         })
     }
-})
+}
 
 //TO DO: Move loops out of the onload as anonymous functions and declare them as functions. Per James:
 // If you want this to look even cleaner, moving all of your for loops out of the onload and making 
