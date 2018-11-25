a6a7ff69607c6e931c7a56efe3801b8fff0aa1c1
Zeke <zeke.cyr@comcast.net>
2018-11-24 18:08:27 -0700

    various generation and event listener assignation methods condensed, color picker implemented

--- script.js
+++ script.js
@@ -9,8 +9,6 @@ var canvasLength = window.prompt("How long?", 20)
 document.addEventListener("DOMContentLoaded", function(){
     generateCanvas()
     generatePalette()
-    assignClickListeners()
-    assignPaletteListeners()
     assignColorPickerListener()
     document.addEventListener("mouseup", function(){
         clickHeld = false
@@ -22,7 +20,8 @@ function generateCanvas(){
         var rowGen = document.createElement("div")
         for(let j = 0;j < canvasWidth;j++){
             let tempBox = document.createElement("div")
-            tempBox.classList.add("pixel","white")
+            tempBox.classList.add("pixel")
+            tempBox.style.backgroundColor = currentColor
             tempBox.addEventListener("mousedown", function(){
                 clickHeld = true
             })
@@ -30,12 +29,24 @@ function generateCanvas(){
         }
         canvasHolder.appendChild(rowGen)
     }
+    for(let i = 0;i < canvasHolder.getElementsByClassName("pixel").length;i++){
+        canvasHolder.getElementsByClassName("pixel")[i].addEventListener("mousedown", (event) =>{
+            event.target.style.backgroundColor = currentColor
+        })
+        canvasHolder.getElementsByClassName("pixel")[i].addEventListener("mouseover", (event) => {
+            if(clickHeld){
+                event.target.style.backgroundColor = currentColor
+
+            }
+        })
+    }
 }
 
 function generatePalette(){
     for(let i = 0;i < colorsToAdd.length;i++){
         var newColor = document.createElement("div")
-        newColor.classList.add("color",colorsToAdd[i])
+        newColor.classList.add("color")
+        newColor.style.backgroundColor = colorsToAdd[i]
         paletteHolder.appendChild(newColor)
     }
     var indicator = document.createElement("p")
@@ -43,44 +54,28 @@ function generatePalette(){
     indicator.innerText = "Current Brush Color: "
     paletteHolder.appendChild(indicator)
     var colorDisplay = document.createElement("div")
-    colorDisplay.classList.add("color", currentColor)
+    colorDisplay.classList.add("color")
+    colorDisplay.style.backgroundColor = currentColor
     colorDisplay.id = "colorDisplay"
     colorDisplay.addEventListener("onchange", function(){
         colorDisplay.classList[1] = currentColor
     })
     paletteHolder.appendChild(colorDisplay)
-}
-
-function assignClickListeners(){
-    for(let i = 0;i < canvasHolder.getElementsByClassName("pixel").length;i++){
-        canvasHolder.getElementsByClassName("pixel")[i].addEventListener("mousedown", function() {
-            var temp = canvasHolder.getElementsByClassName("pixel")[i]
-            temp.classList.replace(temp.classList[1],currentColor)
-        })
-        canvasHolder.getElementsByClassName("pixel")[i].addEventListener("mouseover", function() {
-            if(clickHeld){
-                var temp = canvasHolder.getElementsByClassName("pixel")[i]
-                temp.classList.replace(temp.classList[1],currentColor)
-            }
-        })
-    }
-}
-
-function assignPaletteListeners(){
     for(let i = 0;i < paletteHolder.getElementsByClassName("color").length;i++){
         paletteHolder.getElementsByClassName("color")[i].addEventListener("click", function() {
             var temp = paletteHolder.getElementsByClassName("color")[i]
-            currentColor = temp.classList[1]
+            currentColor = temp.style.backgroundColor
             var display = document.getElementById("colorDisplay")
-            display.classList.replace(display.classList[1],currentColor)
+            display.style.backgroundColor = currentColor
         })
     }
 }
 
 function assignColorPickerListener(){
     var pickerListener = document.getElementById("colorPicker")
-    pickerListener.addEventListener("change", function(){
-        currentColor = pickerListener.nodeValue
+    pickerListener.addEventListener("change", (event) =>{
+        currentColor = event.target.value
+        document.getElementById("colorDisplay").style.backgroundColor = currentColor
     })
 }
 
